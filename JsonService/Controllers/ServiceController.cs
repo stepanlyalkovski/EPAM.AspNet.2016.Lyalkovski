9997aad99447fa42b8e79e49d7e294fefcdc5549
stepanlyalkovski <stepanlyalkovski@gmail.com>
2016-08-31 12:52:37 +0300

    add project with demo routes

---
+++ JsonService/Controllers/ServiceController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace JsonService.Controllers
+{
+    public class ServiceController : Controller
+    {
+        public JsonResult Index()
+        {
+            var user = new {name = "Ivan", Age = 20};
+            return Json(user, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ WebApp/App_Start/RouteConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+namespace WebApp
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "JsonService",
+                url: "Service/{action}/{id}",
+                defaults: new { controller = "Service", action = "Index", id = UrlParameter.Optional },
+                namespaces:new [] { "JsonService.Controllers" }
+            );
+            /*
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+            */
+
+        }
+    }
+}
