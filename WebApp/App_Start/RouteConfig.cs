062e726ab99bb730032ce1802fa80c90cf3a606d
stepanlyalkovski <stepanlyalkovski@gmail.com>
2016-08-31 13:05:43 +0300

    add static route

--- WebApp/App_Start/RouteConfig.cs
+++ WebApp/App_Start/RouteConfig.cs
@@ -13,18 +13,24 @@ namespace WebApp
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "StaticRoute",
+                url: "Static/Home/{message}",
+                defaults: new { controller = "Static", action = "Index", message = UrlParameter.Optional }
+                );
+
+            routes.MapRoute(
                 name: "JsonService",
                 url: "Service/{action}/{id}",
                 defaults: new { controller = "Service", action = "Index", id = UrlParameter.Optional },
                 namespaces:new [] { "JsonService.Controllers" }
             );
-            /*
+            
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
-            */
+            
 
         }
     }
---
+++ WebApp/Controllers/StaticController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class StaticController : Controller
+    {
+        // GET: Static
+        public ActionResult Index()
+        {
+            ViewBag.Message = RouteData.Values["message"];
+            return View();
+        }
+    }
+}
